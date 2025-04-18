9a14f534a4dfb3e9bb3ab3d5a76b8433eef6f2f4
erenezen <eren.ezen@yandex.com>
2025-04-18 06:08:00 +0300

    Add files via upload

---
+++ top_interrupt_tb.vhd
@@ -0,0 +1,64 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 12.09.2019 16:47:09
+-- Design Name: 
+-- Module Name: sim_top - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity sim_top is
+--  Port ( );
+end sim_top;
+
+architecture Behavioral of sim_top is
+component top is
+    Port ( clk : in STD_LOGIC;
+           in_port : in STD_LOGIC_VECTOR (7 downto 0);
+           port_id : out STD_LOGIC_VECTOR (7 downto 0));
+end component top;
+
+signal clk :  STD_LOGIC;
+signal in_port :  STD_LOGIC_VECTOR (7 downto 0);
+signal port_id :  STD_LOGIC_VECTOR (7 downto 0);
+
+begin
+
+uut: top port map(
+                clk => clk,
+                in_port => in_port,
+                port_id => port_id
+);
+process
+begin
+    clk <= '0';
+    wait for 5ns;
+    clk <= '1';
+    wait for 5ns;
+end process;
+
+end Behavioral;
